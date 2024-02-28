0b42c97108c1f3df172bc06f12e2afa86069b2c5
Espaniiol <guilhermeespaniol@gmail.com>
2024-02-27 22:47:18 -0300

    Sua mensagem de commit aqui

---
+++ Estoque/Estoque.dpr
@@ -0,0 +1,14 @@
+program Estoque;
+
+uses
+  Vcl.Forms,
+  unitPrincipal in 'unitPrincipal.pas' {FormPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFormPrincipal, FormPrincipal);
+  Application.Run;
+end.
